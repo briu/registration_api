5aa6b05678ce9c9bbb763f27767863a012498971
Igor <you@example.com>
2013-11-10 10:22:31 +0400

    mapping problem

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::RegistrationsController < Api::V1::ApiController
     @fb = params[:user][:fb_id]
     @twt = params[:user][:fb_id]
     if @fb.present? || @twt.present?
-      user = User.new(params[:user])
-      if user.save
-        sign_in user
-        render json: { success: true, auth_token: resource.authentication_token }, status: :created
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        render json: { success: true, auth_token: @user.authentication_token }, status: :created
       else
         warden.custom_failure!
         render :json=> user.errors, :status=>422
