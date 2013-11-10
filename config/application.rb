35ebe1e52d3180a0f32ae3ca1da76b19de6c0622
Igor <you@example.com>
2013-11-10 06:18:27 +0400

    some config

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,7 @@ module RegistrationApi
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
     config.assets.initialize_on_precompile = false
+    config.middleware.use config.session_store, config.session_options
 
     # Enable the asset pipeline
     config.assets.enabled = true
