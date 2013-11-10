ccf8423c0862720181ae016005b8f848ea4d41d4
Igor <you@example.com>
2013-11-10 12:57:49 +0400

    migrate changing

--- db/migrate/20131109072131_remove_token_authenticatable_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTokenAuthenticatableFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :token_authenticatable, :string
-  end
-
-  def down
-    add_column :users, :token_authenticatable, :string
-  end
-end
